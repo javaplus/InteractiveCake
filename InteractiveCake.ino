36f153096ee2ea57e73f9bee7a94c2b1cb707cdb
Barry <btarlton@gmail.com>
2019-12-12 19:30:04 -0500

    use constant for max hit

--- InteractiveCake.ino
+++ InteractiveCake.ino
@@ -15,7 +15,8 @@ int SOLENOID = 5; //the pin we connect the Solenoid
 int flickerInterval = 100;
 unsigned long startFlickerTime = 0;
 const int VIBRATION_LEVEL_FOR_HIT=50;
-int countDownValue = 10;
+const int HITS_TO_ACTIVATE = 3;
+int countDownValue = HITS_TO_ACTIVATE;
 
 unsigned long lastHit = 0;
 unsigned long timeToReactToHit = 1500;
@@ -71,7 +72,7 @@ void loop()
       delay(150);
       digitalWrite(SOLENOID, LOW); 
       
-      countDownValue = 10;
+      countDownValue = HITS_TO_ACTIVATE;
       matrix.print(countDownValue);
       matrix.writeDisplay();
     }
